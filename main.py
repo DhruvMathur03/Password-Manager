049ccafc48899d4a8d8ce071fdeec31496f90409
Dhruv Mathur <dhruv.mathur03@gmail.com>
2020-08-31 01:26:47 +0530

    address comments

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 import cli_ui
 
+
+def login():
+    usrnm = cli_ui.ask_string("Username")
+    pwd = cli_ui.ask_password("Password")
+
+    if usrnm in dict1 and dict1[usrnm] == pwd:
+        print("Logged in :)")
+    else:
+        print("Wrong Username/Password")
+
+    return
+
+
+def sign_in():
+    email = cli_ui.ask_string("Please enter Email ID")
+
+    if email in lst:
+        print("An account linked to this ID already exists")
+    else:
+        lst.append(email)
+        usrnm1 = cli_ui.ask_string("Please enter username")
+
+        if usrnm1 in dict1:
+            print("Username already exists, please try again")
+        else:
+            pwd1 = cli_ui.ask_password("please enter password")
+            dict1[usrnm1] = pwd1
+
+    return
+
+
 dict1 = {"pugsey": "12345", "dhruv": "123456", "yash": "1234567"}
-lst = {"d@gmail.com", "y@gmail.com", "p@gmail.com"}
+lst = ["d@gmail.com", "y@gmail.com", "p@gmail.com"]
 
 choices = ['Login', 'Sign Up']
 c = cli_ui.ask_choice("Would you like to", choices=choices)
 print(c)
 
 if c == "Login":
-    def login():
-        usrnm = cli_ui.ask_string("Username")
-        if usrnm in dict1:
-            pswd = cli_ui.ask_password("Password")
-            if pswd in dict1:
-                print("Logged in :)")
-            else:
-                print("Wrong Password")
-        else:
-            print("Incorrect username")
-        return
-
-if c == "Sign Up":
-    def sign_in():
-        eml = cli_ui.ask_string("Please enter Email ID")
-        if eml in lst:
-            print("An account linked to this ID already exists")
-
-        else:
-
-            usrnm1 = cli_ui.ask_string("Please enter username")
-            if usrnm1 in dict1:
-                print("Username already exists, please try again")
-            else:
-                pwd1 = cli_ui.ask_password("please enter password")
-                dict1[usrnm1] = pwd1
-        return
-login()
+    login()
+elif c == "Sign Up":
+    sign_in()
